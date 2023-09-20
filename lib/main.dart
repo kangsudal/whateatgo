767237e12dce366ad932588fb9d0f366ed0932f5
kangsudal <kangsudal@gmail.com>
2023-09-20 23:15:34 +0900

    데이터 모델 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,44 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whateatgo/screen/home_screen.dart';
 
+import 'model/recipe.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    fetchData();
     return const MaterialApp(
       home: HomeScreen(),
     );
+  }
+
+  void fetchData() async {
+    //로컬 파일로부터 모든 레시피를 불러옴
+    try {
+      //파일을 읽어와 List<Recipe>로 변환시킨다.
+      String jsonString = await rootBundle.loadString('sourceFile');
+      Map<String, dynamic> dataMap = jsonDecode(jsonString);
+      List<dynamic> dataList = dataMap['COOKRCP01']['row'];
+      List<Recipe> recipes =
+          dataList.map((json) => Recipe.fromJson(json)).toList();
+
+      //TODO: recipes 상태관리 변수에 넣기
+    } catch (e) {
+      throw Exception(e);
+    }
   }
 }
