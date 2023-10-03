7168e99e7367d3f130149cc3d4d10ee83677e62b
kangsudal_notebook <kangsudal@gmail.com>
2023-10-03 20:55:06 +0900

    전체리스트 페이지 보고있을때 shake안되도록

--- lib/screen/list_screen.dart
+++ lib/screen/list_screen.dart
@@ -5,11 +5,31 @@ import 'package:whateatgo/screen/manual_screen.dart';
 
 import '../model/recipe.dart';
 
-class ListScreen extends ConsumerWidget {
+class ListScreen extends ConsumerStatefulWidget {
   const ListScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends ConsumerState<ListScreen> {
+  @override
+  void initState() {
+    //홈화면으로부터 벗어나 manual화면으로 들어왔을때 잠시 shakeDetector를 끈다.
+    ref.read(shakeDetectorProvider.notifier).state.stopListening();
+    super.initState();
+  }
+
+  @override
+  void deactivate() {
+    //State 객체가 Flutter의 구성 트리로부터 제거될 때 호출
+    //다시 홈화면으로 갈땐 shakeDetector를 킨다.
+    ref.read(shakeDetectorProvider.notifier).state.startListening();
+    super.deactivate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     List<Recipe> items = ref.watch(filteredRecipesProvider);
     final TextEditingController myController = TextEditingController();
     return Scaffold(
